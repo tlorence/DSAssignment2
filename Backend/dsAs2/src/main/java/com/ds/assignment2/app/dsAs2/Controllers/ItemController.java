51ddefa32a738b999c108978949dd95fac04308a
Terone Lorence <tirone.lorence@gmail.com>
2021-05-22 18:11:51 +0530

    Context endpoint added

--- Backend/dsAs2/src/main/java/com/ds/assignment2/app/dsAs2/Controllers/ItemController.java
+++ Backend/dsAs2/src/main/java/com/ds/assignment2/app/dsAs2/Controllers/ItemController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/items")
 public class ItemController {
     @Autowired
     private ItemRepository repository;
