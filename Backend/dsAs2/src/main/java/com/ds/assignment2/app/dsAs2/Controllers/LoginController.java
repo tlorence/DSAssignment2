5559f6409a3c0b8b77ad31551238dc3759ce0a48
Savvy9899 <pamuditha.rasanjana99@gmail.com>
2021-05-22 22:03:34 +0530

    abcd

--- Backend/dsAs2/src/main/java/com/ds/assignment2/app/dsAs2/Controllers/LoginController.java
+++ Backend/dsAs2/src/main/java/com/ds/assignment2/app/dsAs2/Controllers/LoginController.java
@@ -1,10 +1,13 @@
 package com.ds.assignment2.app.dsAs2.Controllers;
 
+
 import com.ds.assignment2.app.dsAs2.Model.User;
 import com.ds.assignment2.app.dsAs2.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.stylesheets.LinkStyle;
 
+import java.awt.*;
 import java.util.Optional;
 
 @RestController
@@ -16,17 +19,23 @@ public class LoginController {
     private UserRepository repository;
 
     @PostMapping("/auth")
-    public Object validate(@RequestBody User user) {
+    public String validate(@RequestBody User user) {
 
         String username = user.getEmail();
         String password = user.getPassword();
 
+        System.out.println(username);
+        System.out.println(password);
+
         Optional<User> valUser = repository.findById(user.getEmail());
 
+        List userDetails = null;
+
+
         if ((valUser.get().getEmail().equals(username) && valUser.get().getPassword().equals(password))) {
-            return repository.findById(username);
+            return valUser.get().getUserType();
         }
-        else return null;
+        else return "Unauthorized";
 
     }
 }
