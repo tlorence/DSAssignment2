58a0dfae3c5e8daa0de7c9b705959bca31df87ad
Terone Lorence <tirone.lorence@gmail.com>
2021-05-22 22:25:21 +0530

    Role and email sent at login

--- Backend/dsAs2/src/main/java/com/ds/assignment2/app/dsAs2/Controllers/LoginController.java
+++ Backend/dsAs2/src/main/java/com/ds/assignment2/app/dsAs2/Controllers/LoginController.java
@@ -5,9 +5,9 @@ import com.ds.assignment2.app.dsAs2.Model.User;
 import com.ds.assignment2.app.dsAs2.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.awt.*;
+import java.util.HashMap;
 import java.util.Optional;
 
 @RestController
@@ -19,21 +19,21 @@ public class LoginController {
     private UserRepository repository;
 
     @PostMapping("/auth")
-    public String validate(@RequestBody User user) {
+    public Object validate(@RequestBody User user) {
 
         String username = user.getEmail();
         String password = user.getPassword();
-
-        System.out.println(username);
-        System.out.println(password);
 
         Optional<User> valUser = repository.findById(user.getEmail());
 
         List userDetails = null;
 
-
         if ((valUser.get().getEmail().equals(username) && valUser.get().getPassword().equals(password))) {
-            return valUser.get().getUserType();
+            HashMap<String,String> hashMap = new HashMap<String, String>();
+
+            hashMap.put("email", username);
+            hashMap.put("role",valUser.get().getUserType());
+            return hashMap;
         }
         else return "Unauthorized";
 
